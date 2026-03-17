570e41fb0a1d88f29b4bad3a690b9e187173e377
Aydan Şərifova <aydansrifova@Aydans-MacBook-Air.local>
2026-03-18 00:24:59 +0400

    add lesson 04

---
+++ 04.Access Modifires, Encupsulation, Namespace/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _04.Access_Modifires__Encupsulation__Namespace;
+
+    class User
+    {
+        public string Username;
+        private string Password;
+        protected string Email;
+        internal int Id;
+
+        public void SetPassword(string password)
+        {
+            Password = password;
+        }
+
+        public string GetPassword()
+        {
+            return Password;
+        }
+
+        public void ShowUser()
+        {
+            Console.WriteLine("Username: " + Username);
+            Console.WriteLine("Password: " + Password);
+            Console.WriteLine("Email: " + Email);
+            Console.WriteLine("Id: " + Id);
+        }
+    }
+
+    class Admin : User
+    {
+        public void SetEmail(string email)
+        {
+            Email = email;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Admin admin = new Admin();
+
+            admin.Username = "admin01";
+            admin.Id = 1;
+            admin.SetPassword("12345");
+            admin.SetEmail("admin@gmail.com");
+
+            admin.ShowUser();
+        }
+    }
+
+
